7da5786241c7a111e1c8262e13e5bb37a857dde4
20151998@student.hust.edu.vn <20151998@student.hust.edu.vn>
2018-11-11 16:31:09 +0700

    Sua giao dien

--- source/resources/views/admin/elements/master.blade.php
+++ source/resources/views/admin/elements/master.blade.php
@@ -49,11 +49,7 @@
 
 					@yield('content')
 
-					<!-- Footer -->
-					<div class="footer text-muted">
-						© 2015. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
-					</div>
-					<!-- /footer -->
+
 					
 				</div>
 				<!-- /content area -->
